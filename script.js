ced759a1b07dff4d28f4d36146e06eb9824ed430
mohammadyaksafu <mohammadyaksafu@gmail.com>
2025-01-26 23:45:15 +0600

    full complete

--- script.js
+++ script.js
@@ -15,14 +15,27 @@ function addTask(){
 
     }
     inputbox.value="";
+    saveData();
 }
 
 listcontainer.addEventListener("click",function(e){
     if(e.target.tagName==="LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }else if(e.target.tagName==="SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 },false);
+
+
+function saveData(){
+    localStorage.setItem("data",listcontainer,innerHTML)
+}
+
+function showTask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
 
 
